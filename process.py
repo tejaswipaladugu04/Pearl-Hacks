09699f01a354f9cfc62f3fea414b8999b7b61697
rujula-yete <70672062+rujula-yete@users.noreply.github.com>
2024-02-24 19:17:25 -0500

    proces wuop

--- process.py
+++ process.py
@@ -4,16 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
 def process():
     data = request.get_json()
-    input_text = data['inputText']
+    input_text = data['stressLevel']
 
     # Process the input text (replace this with your actual processing logic)
     # processed_text = f'Processed: {input_text.upper()}'
-    processed_text = input_text.upper()
+    if int(input_text) >5:
+        processed_text = 'wow ur really stressed'
+    else:
+        processed_text = 'nice ur not too stressed'
 
     return jsonify({'result':processed_text})
 
